1a3604cc896ffaacef0520176d2eb31850514feb
pujaKundu <pujakundu797@gmail.com>
2023-10-12 09:51:34 +0600

    temp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './components/Home/Home'
+import Layout from './components/Temp/Layout';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Layout/>
+      {/* <Home /> */}
     </div>
   );
 }
